21b22ead1ca3412d4d09cbe4e5e63f1dfdb0b889
Wlodek-YouCanDev <wlodek@youcandev.com>
2024-01-20 23:47:30 +0100

    creating experience section and adding translation for polish and english

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
+//Sidepanel left offset counter
+function adjustSidePanelPosition(language) {
+    const sidePanel = document.querySelector('.sidepanel');
+
+    let leftOffset;
+
+    switch (language) {
+        case 'en':
+            leftOffset = '-85px';
+            break;
+        case 'pl':
+            leftOffset = '-130px';
+            break;
+        case 'ua':
+            leftOffset = '-100px';
+            break;
+    }
+
+    sidePanel.style.left = leftOffset;
+}
+
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElement = document.querySelector('.menu__close');
@@ -9,3 +31,34 @@ hamburger.addEventListener('click', () => {
 closeElement.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+//Text translation
+function translate(language) {
+    document.querySelectorAll('[data-lang-en]').forEach(elem => {
+      elem.textContent = elem.getAttribute(`data-lang-${language}`);
+    });
+  }
+  
+  document.querySelectorAll('.language-select__option').forEach(option => {
+    option.addEventListener('click', function() {
+        const selectedLanguage = this.getAttribute('data-value');
+        const selectedLanguageText = this.textContent;
+
+        translate(selectedLanguage);
+        adjustSidePanelPosition(selectedLanguage);
+
+        const selectedLangElement = document.querySelector('.language-select .selected-lang');;
+        selectedLangElement.textContent = selectedLanguageText;
+
+        document.querySelector('.language-select__options').style.display = 'none';
+        document.querySelector('.selected-lang').style.removeProperty('display');
+    });
+});
+
+document.querySelector('.selected-lang').addEventListener('click', function() {
+    const options = document.querySelector('.language-select__options');
+    const isOptionsVisible = options.style.display === 'block';
+    options.style.display = isOptionsVisible ? 'none' : 'block';
+    this.style.display = 'none';
+});
+
